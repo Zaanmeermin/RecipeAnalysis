47813f944dcd429f0c4fb04cf57d56465f9c1843
Semma Raadschelders <semma2@live.nl>
2022-01-17 14:48:29 +0100

    readCSV function implemented

--- conversion.py
+++ conversion.py
@@ -11,3 +11,20 @@ def makeCSV(fileName, dict, columns):
             row = {'word': word, 'total frequency': freq}
             row.update(dict[word])
             writer.writerow(row)
+
+# Convert from csv to dictionary.
+def readCSV(fileName):
+    wordFreq = {}
+    with open(fileName, mode='r') as inp:
+        reader = csv.reader(inp)
+        columns = reader.__next__()
+        for row in reader:
+            wordFreq[row[0]] = {}
+            print(row)
+            for i, item in enumerate(row[1:]):
+                if(not item == ""):
+                    wordFreq[row[0]][columns[i + 1]] = item 
+
+    print(wordFreq)
+
+readCSV('wordFrequenciesClean.csv')
